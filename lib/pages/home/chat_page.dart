ecdaca7dcaf0c0ea7f0db6a60ab000fb39638ec0
riyanputraf <riyanputrafirjatullah@gmail.com>
2024-05-23 18:01:20 +0700

    feat: chat page empty chat
    Add empty chat UI in chat page

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,12 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/theme.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
 
+  Widget header() {
+    return AppBar(
+      backgroundColor: bgColor1,
+      centerTitle: true,
+      title: Text(
+        'Message Support',
+        style: primaryTextStyle.copyWith(
+          fontWeight: medium,
+          fontSize: 18,
+        ),
+      ),
+      elevation: 0,
+      automaticallyImplyLeading: false,
+    );
+  }
+
+  Widget content() {
+    return Expanded(
+      child: Container(
+        color: bgColor3,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icon_headset.png',
+              width: 80,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Opss no message yet?',
+              style: primaryTextStyle.copyWith(
+                fontWeight: medium,
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              'You have never done a transaction',
+              style: secondaryTextStyle.copyWith(
+                fontSize: 14,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 44,
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 10,
+                  ),
+                  backgroundColor: primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Text(
+                  'Explore Store',
+                  style: primaryTextStyle.copyWith(
+                    fontWeight: medium,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Chat Page'),
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
