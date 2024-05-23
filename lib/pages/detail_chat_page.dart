cb815c3d994c9b3a51433f05e665e773c9b184af
riyanputraf <riyanputrafirjatullah@gmail.com>
2024-05-23 23:26:37 +0700

    feat: add product preview detail chat page
    Add product preview UI in detail chat page

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -25,38 +25,106 @@ class DetailChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget chatInput() {
+    Widget productPreview() {
       return Container(
-        margin: EdgeInsets.all(20),
+        width: 225,
+        height: 74,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.only(
+          bottom: 20,
+        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: bgColor5,
+            border: Border.all(
+              color: primaryColor,
+            )),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: Container(
-                height: 45,
-                padding: EdgeInsets.symmetric(
-                  horizontal: 16,
-                ),
-                decoration: BoxDecoration(
-                  color: bgColor4,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Center(
-                  child: TextFormField(
-                    decoration: InputDecoration.collapsed(
-                      hintText: 'Type Message...',
-                      hintStyle: subtitleTextStyle,
-                    ),
-                  ),
-                ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/image_shoes.png',
+                width: 54,
               ),
             ),
             SizedBox(
-              width: 20,
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'COURT VISION 2.0 dadadadadada',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: regular,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    '\$57,15',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  ),
+                ],
+              ),
             ),
             Image.asset(
-              'assets/button_send.png',
-              width: 45,
-            )
+              'assets/button_close.png',
+              width: 22,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            productPreview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      color: bgColor4,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type Message...',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Image.asset(
+                  'assets/button_send.png',
+                  width: 45,
+                )
+              ],
+            ),
           ],
         ),
       );
--- lib/theme.dart
+++ lib/theme.dart
@@ -14,6 +14,7 @@ Color bgColor1 = Color(0xff1F1D2B);
 Color bgColor2 = Color(0xff2B2937);
 Color bgColor3 = Color(0xff242231);
 Color bgColor4 = Color(0xff252836);
+Color bgColor5 = Color(0xff2B2844);
 
 Color primaryTextColor = Color(0xffF1F0F2);
 Color secondaryTextColor = Color(0xff999999);
