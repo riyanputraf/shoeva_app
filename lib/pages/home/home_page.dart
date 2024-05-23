d40c6179b947df8b522f645249eabddbd764d2f4
riyanputraf <riyanputrafirjatullah@gmail.com>
2024-05-23 14:49:08 +0700

    feat: create header in home page
    Add UI header in Home Page

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  Widget header() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: defaultMargin,
+        left: defaultMargin,
+        right: defaultMargin,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Halo, Riyan',
+                  style: primaryTextStyle.copyWith(
+                    fontWeight: semibold,
+                    fontSize: 24,
+                  ),
+                ),
+                Text(
+                  '@riyanputraf',
+                  style: subtitleTextStyle.copyWith(
+                    fontWeight: regular,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 54,
+            height: 54,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/image_profile.png',
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
