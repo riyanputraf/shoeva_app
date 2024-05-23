ba6a088038d4ca516558cf6613840be6dbbb289d
riyanputraf <riyanputrafirjatullah@gmail.com>
2024-05-23 22:49:45 +0700

    feat: add input section detail chat page
    Add input chat message section in detail chat page

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -5,26 +5,63 @@ class DetailChatPage extends StatelessWidget {
   const DetailChatPage({super.key});
 
   /// Refactor Later
-  Widget header() {
-    return PreferredSize(
-      child: AppBar(
-        backgroundColor: bgColor1,
-        centerTitle: false,
-        title: Row(
-          children: [
-            Image.asset(
-              'assets/image_shop_online.png',
-              width: 50,
-            )
-          ],
-        ),
-      ),
-      preferredSize: Size.fromHeight(70),
-    );
-  }
+  // Widget header() {
+  //   return PreferredSize(
+  //     child: AppBar(
+  //       backgroundColor: bgColor1,
+  //       centerTitle: false,
+  //       title: Row(
+  //         children: [
+  //           Image.asset(
+  //             'assets/image_shop_online.png',
+  //             width: 50,
+  //           )
+  //         ],
+  //       ),
+  //     ),
+  //     preferredSize: Size.fromHeight(70),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
+    Widget chatInput() {
+      return Container(
+        margin: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 45,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: bgColor4,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: TextFormField(
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Type Message...',
+                      hintStyle: subtitleTextStyle,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Image.asset(
+              'assets/button_send.png',
+              width: 45,
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: bgColor3,
       appBar: PreferredSize(
@@ -64,6 +101,7 @@ class DetailChatPage extends StatelessWidget {
         ),
         preferredSize: Size.fromHeight(70),
       ),
+      bottomNavigationBar: chatInput(),
     );
   }
 }
