01773fbd31d3cd4beea936c5afc5be55762d2e20
riyanputraf <riyanputrafirjatullah@gmail.com>
2024-05-24 20:55:48 +0700

    feat: header profile page
    Add header section in profile page

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -1,12 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/theme.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
+    Widget header() {
+      return AppBar(
+        backgroundColor: bgColor1,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(
+              defaultMargin,
+            ),
+            child: Row(
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    'assets/image_profile.png',
+                    width: 64,
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello, Riyan',
+                        style: primaryTextStyle.copyWith(
+                          fontWeight: semibold,
+                          fontSize: 24,
+                        ),
+                      ),
+                      Text(
+                        '@riyanputraf',
+                        style: subtitleTextStyle.copyWith(
+                          fontWeight: regular,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  'assets/button_exit.png',
+                  width: 20,
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+      ],
     );
   }
 }
