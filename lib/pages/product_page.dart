ed39d795d0a3de561a9473bfbb4364b7004d9200
riyanputraf <riyanputrafirjatullah@gmail.com>
2024-05-26 14:54:15 +0700

    feat: add button a chat and add to cart
    Add button a chat and add to cart in detail product page

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -257,12 +257,54 @@ class _ProductPageState extends State<ProductPage> {
                       children: familiarShoes.map((image) {
                         index++;
                         return Container(
-                          margin: EdgeInsets.only(left: index == 0 ? defaultMargin : 0),
+                          margin: EdgeInsets.only(
+                              left: index == 0 ? defaultMargin : 0),
                           child: familiarShoesCard(image),
                         );
                       }).toList(),
                     ),
                   )
+                ],
+              ),
+            ),
+
+            /// Note Buttons
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.all(defaultMargin),
+              child: Row(
+                children: [
+                  Container(
+                    width: 54,
+                    height: 54,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/button_chat.png'))),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 54,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          backgroundColor: primaryColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        child: Text(
+                          'Add to Cart',
+                          style: primaryTextStyle.copyWith(
+                            fontWeight: semibold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
