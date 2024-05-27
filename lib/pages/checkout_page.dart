f38863220621caa7b0f64c7a7836f0144c49e8bf
riyanputraf <riyanputrafirjatullah@gmail.com>
2024-05-27 22:31:34 +0700

    feat: create payment summary in checkout page
    Create payment summary in checkout page

--- lib/pages/checkout_page.dart
+++ lib/pages/checkout_page.dart
@@ -117,7 +117,9 @@ class CheckoutPage extends StatelessWidget {
                               fontSize: 14,
                             ),
                           ),
-                          SizedBox(height: defaultMargin,),
+                          SizedBox(
+                            height: defaultMargin,
+                          ),
                           Text(
                             'Your Address',
                             style: secondaryTextStyle.copyWith(
@@ -137,6 +139,118 @@ class CheckoutPage extends StatelessWidget {
                     )
                   ],
                 ),
+              ],
+            ),
+          ),
+
+          /// Note Payment Summary
+          Container(
+            margin: EdgeInsets.only(
+              top: defaultMargin,
+            ),
+            decoration: BoxDecoration(
+              color: bgColor4,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Payment Summary',
+                  style: primaryTextStyle.copyWith(
+                    fontWeight: medium,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Product Quantity',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(
+                      '2 Items',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Product Price',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(
+                      '\$575.96',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Shipping',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(
+                      'Free',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 12,),
+                Divider(
+                  thickness: 1,
+                  color: subtitleTextColor,
+                ),
+                SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total',
+                      style: priceTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: semibold,
+                      ),
+                    ),
+                    Text(
+                      'Free',
+                      style: priceTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: semibold,
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
           ),
