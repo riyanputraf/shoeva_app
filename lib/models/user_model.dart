f34cd48e37c6f723b416ac56340e6d9d4da1e52d
riyanputraf <riyanputrafirjatullah@gmail.com>
2024-05-28 13:00:52 +0700

    feat: add user model and package
    add user model, provider, and http package

---
+++ lib/models/user_model.dart
@@ -0,0 +1,36 @@
+class UserModel {
+
+  int id;
+  String name;
+  String email;
+  String username;
+  String profilePhotoUrl;
+  String token;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.username,
+    required this.profilePhotoUrl,
+    required this.token,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+    id: json["id"],
+    name: json["name"],
+    email: json["email"],
+    username: json["username"],
+    profilePhotoUrl: json["profile_photo_url"],
+    token: json["token"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "email": email,
+    "username": username,
+    "profile_photo_url": profilePhotoUrl,
+    "token": token,
+  };
+}
