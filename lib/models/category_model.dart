54fe62149b2db84493641f40df478ec77b57a723
riyanputraf <riyanputrafirjatullah@gmail.com>
2024-05-29 08:39:56 +0700

    feat: create model product, category, and gallery
    create model for product, category, and gallery

---
+++ lib/models/category_model.dart
@@ -0,0 +1,21 @@
+class CategoryModel{
+
+  int id;
+  String name;
+
+  CategoryModel({
+    required this.id,
+    required this.name,
+  });
+
+  factory CategoryModel.fromJson(Map<String, dynamic> json) => CategoryModel(
+    id: json["id"],
+    name: json["name"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+  };
+
+}
---
+++ lib/models/gallery_model.dart
@@ -0,0 +1,21 @@
+class GalleryModel{
+
+  int id;
+  String url;
+
+  GalleryModel({
+    required this.id,
+    required this.url,
+  });
+
+  factory GalleryModel.fromJson(Map<String, dynamic> json) => GalleryModel(
+    id: json["id"],
+    url: json["url"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "url": url,
+  };
+
+}
